066cf9d5dafe4a5db0b6168ddd9b9eaa5badba5e
Munira <79731097+Munira-mr@users.noreply.github.com>
2022-11-10 18:06:17 -0500

    Update SalaryCalculator.java

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -3,17 +3,17 @@ package school.mjc.stage0.conditions.task3;
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
         if (salary <= 10000||salary<=0) {
-            int tax = (salary / 100) * 15;
-            int newSalary = salary - tax;
+            float tax = (salary / 100) * 15;
+            float newSalary = salary - tax;
 
             System.out.println(newSalary);
         } else if (salary <= 20000||salary>10000) {
-            int tax = (salary / 100) * 18;
-            int newSalary = salary - tax;
+            float tax = (salary / 100) * 18;
+            float newSalary = salary - tax;
             System.out.println(newSalary);
         } else if (salary > 20000) {
-            int tax = (salary / 100) * 20;
-            int newSalary = salary - tax;
+            float tax = (salary / 100) * 20;
+            float newSalary = salary - tax;
             System.out.println(newSalary);
         }else {
             System.out.println("wrong input!");
